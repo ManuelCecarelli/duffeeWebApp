886eef5dbe4de10d27212f0ff601005f54d19375
ManuelCecarelli <manuelcecarelli@gmail.com>
2024-06-22 00:59:22 -0300

    new fixes in Order class

--- src/Domain/Entities/Order.cs
+++ src/Domain/Entities/Order.cs
@@ -7,19 +7,19 @@ namespace Domain.Entities
     { 
         public int Id { get; set; }
 
-        public Product Product { get; set; }
+        public Product RelatedPoduct { get; set; }
 
-        public Client Client { get; set; }
+        public Client RelatedClient { get; set; }
 
         [Column(TypeName = "datetime")]
         public DateTime CreationDate { get; private set; }
 
         public OrderState State { get; private set; }
 
-        public Order(Product product, Client client)
+        public Order(Product relProduct, Client relClient)
         {
-            Product = product;
-            Client = client;
+            RelatedPoduct = relProduct;
+            RelatedClient = relClient;
             CreationDate = DateTime.Now;
             State = OrderState.Pendent;
         }
